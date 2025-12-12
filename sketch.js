e237869d3fc65aab0655331cefb243e073c8efd5
kaurkomal32591-prog <kaurkomal32591@gmail.com>
2025-12-12 19:48:03 +0100

    Update sketch.js
    Add gravity and jumping to player

--- sketch.js
+++ sketch.js
@@ -1,16 +1,39 @@
+let playerX;
+let playerY;
+let velocityY = 0;
+let gravity = 0.6;
+
 function setup() {
   createCanvas(400, 600);
+  playerX = width / 2;
+  playerY = height - 60;
 }
 
 function draw() {
   background(220);
 
-  // Player (temporary circle)
-  fill(0, 200, 0);
-  ellipse(width / 2, height - 60, 40, 40);
+  // gravity
+  velocityY += gravity;
+  playerY += velocityY;
 
+  // ground collision
+  if (playerY > height - 40) {
+    playerY = height - 40;
+    velocityY = 0;
+  }
+
+  // player
+  fill(0, 200, 0);
+  ellipse(playerX, playerY, 40, 40);
+
+  // title
   fill(0);
   textAlign(CENTER);
-  textSize(14);
   text("Doodle Jump – work in progress", width / 2, 30);
+}
+
+function keyPressed() {
+  if (key === ' ') {
+    velocityY = -12; // jump
+  }
 }
